fa86a9d42eeb6e2bf24e3b504b06da5f74970ddc
Malathi-ml <malathimadhu643@gmail.com>
2026-03-19 12:33:13 +0530

    Updated MongoDB connection to Atlas

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 
 /* ✅ CONNECT MONGODB */
-mongoose.connect("mongodb://127.0.0.1:27017/ordersDB")
+mongoose.connect("mongodb+srv://malathimadhu643_db_user:g8oYaFg9akjttYIn@cluster0.9h51dm1.mongodb.net/ordersDB?appName=Cluster0")
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err))
 
